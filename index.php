a162c8fa58a734fda6a40614004eae0426106b98
Adrian Cortes <ls-adrian@hotmail.com>
2021-01-31 15:53:24 -0500

    web-coca-cola

---
+++ index.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Coca-Cola es una bebida gaseosa y refrescante vendida a nivel mundial
+        en tiendas, restaurantes y máquinas expendedoras en más de doscientos países o territorios. 
+        Es un producto de The Coca-Cola Company, de origen estadounidense.">
+        <meta name="keywords" content="bebida, gaseosa,cocacola">
+        <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/ebb4eb86b7.js" crossorigin="anonymous"></script>
+        <title>Inicio - Coca-Cola</title>
+    </head>
+
+    <body>
+        <header>
+            <div class="container">
+                <a href="index.html"><img src="img/logo.svg" class="logo" alt="logo.svg"></a>
+                <nav>
+                    <a href="#inicio"> Inicio </a>
+                    <a href="#nosotros">Nosotros</a>
+                    <a href="#servicios">Servicios</a>
+                    <a href="#galeria">Galeìa</a>
+                    <a href="#contactenos">Contàctenos</a>
+                </nav>
+                <a href="#" class="hamb"><i class="fas fa-bars"></i></a>
+            </div>
+        </header>
+
+        <main>
+            <section id="inicio">
+                <img src="img/bannerprincipal.jpg" class="logo" alt="bannerprincipal">
+                <div class="bloque-inicio">
+                    <h1> Bienvenidos a Coca Cola </h1>
+
+                    <p> Coca-Cola, conocida comúnmente como Coca en muchos países hispanohablantes, 
+                        es una bebida gaseosa y refrescante vendida a nivel mundial en tiendas, 
+                        restaurantes y máquinas expendedoras en más de doscientos países o territorios. 
+                        Es un producto de The Coca-Cola Company, de origen estadounidense.
+                    </p>
+                    <a href="#nosotros" class="boton boton-rojo"> Ver màs </a>
+                </div>
+            </section>
+
+            <section id="nosotros" class="seccion">
+                <div class="container">
+                    <p> 
+                        Coca-Cola, conocida comúnmente como Coca en muchos países hispanohablantes, 
+                        es una bebida gaseosa y refrescante vendida a nivel mundial en tiendas, 
+                        restaurantes y máquinas expendedoras en más de doscientos países o territorios. 
+                        Es un producto de The Coca-Cola Company, de origen estadounidense.
+                    </p>
+                </div>
+            </section>
+
+            <section id="servicios" class="seccion">
+                <div class="container">
+                    <div class="row">
+                        <div class="columna columna-33 columna-mobile-100">
+                            <div class="bloque-servicio">
+                                <div class="bloque-img-servicios cuadrado-perfecto">
+                                    <img src="img/servicio1.jpg" alt="servicio1">
+                                </div>
+                                <div class="bloque-contenido-servicio">
+                                    <h3> Servicio 1 </h3>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                                        Autem facere atque pariatur. Porro, incidunt blanditiis.
+                                    </p>
+                                    <a href="#" class="boton boton-blanco"> Ver Màs </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="columna columna-33 columna-mobile-100">
+                            <div class="bloque-servicio">
+                                <div class="bloque-img-servicios cuadrado-perfecto">
+                                    <img src="img/servicio2.jpg" alt="servicio1">
+                                </div>
+                                <div class="bloque-contenido-servicio">
+                                    <h3> Servicio 2 </h3>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                                        Autem facere atque pariatur. Porro, incidunt blanditiis.
+                                    </p>
+                                    <a href="#" class="boton boton-blanco"> Ver Màs </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="columna columna-33 columna-mobile-100">
+                            <div class="bloque-servicio">
+                                <div class="bloque-img-servicios cuadrado-perfecto">
+                                    <img src="img/servicio3.jpg" alt="servicio1">
+                                </div>
+                                <div class="bloque-contenido-servicio">
+                                    <h3> Servicio 3 </h3>
+                                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                                        Autem facere atque pariatur. Porro, incidunt blanditiis.
+                                    </p>
+                                    <a href="#" class="boton boton-blanco"> Ver Màs </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="galeria">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio1.jpg" alt="servicio1-img1">
+                                <h4> Imagen 1 </h4>
+                            </div>
+                        </div>
+
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio2.jpg" alt="servicio1-img2">
+                                <h4> Imagen 2 </h4>
+                            </div>
+                        </div>
+
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio3.jpg" alt="servicio1-img3">
+                                <h4> Imagen 3 </h4>
+                            </div>
+                        </div>
+
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio1.jpg" alt="servicio1-img4">
+                                <h4> Imagen 4 </h4>
+                            </div>
+                        </div>
+
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio2.jpg" alt="servicio1-img5">
+                                <h4> Imagen 5 </h4>
+                            </div>
+                        </div>
+
+                        <div class="columna-33 columna-mobile-50">
+                            <div class="cuadrado-perfecto">
+                                <img src="img/servicio3.jpg" alt="servicio1-img6">
+                                <h4> Imagen 6 </h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="contactenos" class="seccion">
+                <iframe width="520" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" id="gmap_canvas" 
+                    src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=63%20Medell%C3%ADn+(Colombia)&amp;t=&amp;z=13&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                </iframe>
+                <a href='http://maps-website.com/es'>Maps-Generator.com/es</a> 
+                <script type='text/javascript' src='https://embedmaps.com/google-maps-authorization/script.js?id=c4f2c4dd53234da78879e787d321da700b430539'></script>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="columna columna-41 columna-mobile-100 empujar-58 empujar-mobile-0 sinpadding-mobile">
+
+                            <form action="index.php" method="POST">
+                                <div class="form-block">
+                                    <input type="text" name="nombre" class="form-control" placeholder="Nombre">
+                                </div>
+
+                                <div class="form-block">
+                                    <input type="email" name="email" class="form-control" placeholder="Email">
+                                </div>
+
+                                <div class="form-block">
+                                    <textarea name="mensaje" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
+                                </div>
+
+                                <div class="form-block bloque-ultimo">
+                                    <input type="submit" name="" value="Enviar" class="boton boton-negro">
+                                </div>
+
+                                <?php 
+                                    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+                                        $nombre = $_POST["nombre"];
+                                        $email = $_POST["email"];
+                                        $mensaje = $_POST["mensaje"];
+
+                                        if(isset($nombre)){
+                                            if(isset($email)){
+                                                if(isset($mensaje)){
+                                                    $para = "ls.adrian.co@gmail.com";
+                                                    $asunto = "Esto es una Prueba";
+                                                    $cuerpo = $nombre ."\n". $email ."\n". $mensaje;
+                                                    $adicional = "From: ls.adrian.co@gmail.com";
+
+                                                    mail($para, $asunto, $cuerpo, $adicional);
+                                                    ?>
+                                                        <p> Envio exitoso </p>
+                                                    <?php
+                                                }
+                                            }
+                                        }
+                                    }
+                                ?>
+
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="columna columna-25 columna-mobile-100">
+                        <img src="img/logo-blanco.png" class="logo-footer" alt="logo-blanco">
+                        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus debitis error dolor 
+                            blanditiis facilis quo.
+                        </p>
+                    </div>
+
+                    <div class="columna columna-25 columna-mobile-100">
+                        <h3> Temas relacionados </h3>
+                        <ul>
+                            <li><a href="#"> Tema 1 </a></li>
+                            <li><a href="#"> Tema 2 </a></li>
+                            <li><a href="#"> Tema 3 </a></li>
+                        </ul>
+                    </div>
+
+                    <div class="columna columna-25 columna-mobile-100">
+                        <h3> Datos de Contacto </h3>
+                        <ul>
+                            <li> ls-adrian@hotmail.com </li>
+                            <li> 302 434 95 02 </li>
+                            <li> 
+                                Cr.63 # 111-38 <br>
+                                Medellin, Antioquia, Colombia 
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="columna columna-25 columna-mobile-100">
+                        <h3> Redes Sociales </h3>
+                        <ul class="redes">
+                            <li><a href="#"> <i class="fab fa-facebook-square"></i> </a></li>
+                            <li><a href="#"> <i class="fab fa-twitter-square"></i> </a></li>
+                            <li><a href="#"> <i class="fab fa-instagram-square"></i> </a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="barra-footer">
+                &copy; Derechos Reservados - 2021
+            </div>
+        </footer>
+        <script src="js/jquery.js"></script>
+        <script src="js/funciones.js"></script>
+    </body>
+</html>
